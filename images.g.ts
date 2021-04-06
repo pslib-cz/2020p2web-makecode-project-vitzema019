a90f4c272d27966014c529d36d4aa3a4b0cd433a
vitzema019 <71689674+vitzema019@users.noreply.github.com>
2021-04-06 12:22:52 +0200

    assets

--- images.g.ts
+++ images.g.ts
@@ -60,25 +60,6 @@ namespace myImages {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
-            case "image4":
-            case "arrow1":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . 1 1 1 . . . . 
-. . . . . . . . . . 1 1 . . . . 
-. . . . . . . . . e . 1 . . . . 
-. . . . . . . . e . . . . . . . 
-. . . . . . . e . . . . . . . . 
-. . . . . . e . . . . . . . . . 
-. . . . . e . . . . . . . . . . 
-. . . . e . . . . . . . . . . . 
-. . f f . . . . . . . . . . . . 
-. . . f . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`;
             case "image5":
             case "56 -56":return img`
 . . . . . . . . . . . . . . . . 
@@ -230,6 +211,163 @@ namespace myImages {
 f 4 f 4 4 f f f 4 4 f . . 
 4 4 4 f f f f f f f . . . 
 f f f f . f f f f . . . . 
+`;
+            case "image9":
+            case "arrowLeft":return img`
+. . . . . . . . . . . . . . . . 
+. . 1 . . . . . . . . . . f . . 
+. 1 1 e e e e e e e e e f . . . 
+. . 1 . . . . . . . . . . f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "arrowUp":return img`
+. . . . . 
+. . 1 . . 
+. 1 1 1 . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . f . . 
+. f . f . 
+. . . . . 
+. . . . . 
+`;
+            case "image12":
+            case "arrowDown":return img`
+. . . . . 
+. . . . . 
+. f . f . 
+. . f . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. . e . . 
+. 1 1 1 . 
+. . 1 . . 
+. . . . . 
+`;
+            case "image13":
+            case "arrowRight":return img`
+. . . . . . . . . . . . . . . . 
+. . f . . . . . . . . . . 1 . . 
+. . . f e e e e e e e e e 1 1 . 
+. . f . . . . . . . . . . 1 . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image14":
+            case "arrowNone":return img`
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+`;
+            case "image18":
+            case "Enemy2":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image19":
+            case "Enemy3":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image20":
+            case "Enemy1":return img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd111111111f......
+......fd11111111df......
+......fd11111111df......
+......fcdd1111ddcff.....
+.......fbcf11fcbfbbf....
+.......ffbdb1bdffff.....
+........fcbfbfdf........
+........ffffffff........
+......ffffffffff........
+.....fcb1bcffff.........
+......ffbff.............
+........................
+........................
+........................
+........................
+........................
+`;
+            case "image21":
+            case "Chest":return img`
+. . b b b b b b b b b b b b . . 
+. b e 4 4 4 4 4 4 4 4 4 4 e b . 
+b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+b e e e e e e e e e e e e e e b 
+b e e e e e e e e e e e e e e b 
+b b b b b b b d d b b b b b b b 
+c b b b b b b c c b b b b b b c 
+c c c c c c b c c b c c c c c c 
+b e e e e e c b b c e e e e e b 
+b e e e e e e e e e e e e e e b 
+b c e e e e e e e e e e e e c b 
+b b b b b b b b b b b b b b b b 
+. b b . . . . . . . . . . b b . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,92 +1,17 @@
 tiles.setTilemap(tilemap`level2`)
-let chest = sprites.create(img`
-    . . b b b b b b b b b b b b . .
-    . b e 4 4 4 4 4 4 4 4 4 4 e b .
-    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
-    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
-    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
-    b e e 4 4 4 4 4 4 4 4 4 4 e e b
-    b e e e e e e e e e e e e e e b
-    b e e e e e e e e e e e e e e b
-    b b b b b b b d d b b b b b b b
-    c b b b b b b c c b b b b b b c
-    c c c c c c b c c b c c c c c c
-    b e e e e e c b b c e e e e e b
-    b e e e e e e e e e e e e e e b
-    b c e e e e e e e e e e e e c b
-    b b b b b b b b b b b b b b b b
-    . b b . . . . . . . . . . b b .
-`)
+let chest = sprites.create(assets.image`Chest`)
 let mySprite = sprites.create(assets.image`FOX10`,SpriteKind.Player) //set img of player
 
-let myEnemy = sprites.create(img`
-    ........................
-    ........................
-    ........................
-    ........................
-    ..........ffff..........
-    ........ff1111ff........
-    .......fb111111bf.......
-    .......f11111111f.......
-    ......fd111111111f......
-    ......fd11111111df......
-    ......fd11111111df......
-    ......fcdd1111ddcff.....
-    .......fbcf11fcbfbbf....
-    .......ffbdb1bdffff.....
-    ........fcbfbfdf........
-    ........ffffffff........
-    ......ffffffffff........
-    .....fcb1bcffff.........
-    ......ffbff.............
-    ........................
-    ........................
-    ........................
-    ........................
-    ........................
-`,SpriteKind.Enemy) //set img of enemy
-let myEnemy2 = sprites.create(img`
-    . . . . . . f f f f . . . . . .
-    . . . . f f f 2 2 f f f . . . .
-    . . . f f f 2 2 2 2 f f f . . .
-    . . f f f e e e e e e f f f . .
-    . . f f e 2 2 2 2 2 2 e e f . .
-    . . f e 2 f f f f f f 2 e f . .
-    . . f f f f e e e e f f f f . .
-    . f f e f b f 4 4 f b f e f f .
-    . f e e 4 1 f d d f 1 4 e e f .
-    . . f e e d d d d d d e e f . .
-    . . . f e e 4 4 4 4 e e f . . .
-    . . e 4 f 2 2 2 2 2 2 f 4 e . .
-    . . 4 d f 2 2 2 2 2 2 f d 4 . .
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
-    . . . . . f f f f f f . . . . .
-    . . . . . f f . . f f . . . . .
-`,SpriteKind.Enemy)
+let myEnemy = sprites.create(assets.image`Enemy1`,SpriteKind.Enemy) //set img of enemy
+let myEnemy2 = sprites.create(assets.image`Enemy2`,SpriteKind.Enemy)
 //arrow variables
-let myEnemy3 = sprites.create(img`
-    . . . . . . f f f f . . . . . .
-    . . . . f f f 2 2 f f f . . . .
-    . . . f f f 2 2 2 2 f f f . . .
-    . . f f f e e e e e e f f f . .
-    . . f f e 2 2 2 2 2 2 e e f . .
-    . . f e 2 f f f f f f 2 e f . .
-    . . f f f f e e e e f f f f . .
-    . f f e f b f 4 4 f b f e f f .
-    . f e e 4 1 f d d f 1 4 e e f .
-    . . f e e d d d d d d e e f . .
-    . . . f e e 4 4 4 4 e e f . . .
-    . . e 4 f 2 2 2 2 2 2 f 4 e . .
-    . . 4 d f 2 2 2 2 2 2 f d 4 . .
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
-    . . . . . f f f f f f . . . . .
-    . . . . . f f . . f f . . . . .
-            `,SpriteKind.Enemy)
+let myEnemy3 = sprites.create(assets.image`Enemy3`,SpriteKind.Enemy)
             myEnemy3.setPosition(-410, -400)
 //------------------------------------------------------
 let arrow = 1      //default value of arrow in inventory.
 let addarrow = 0
 let spawnEnemy2 = false
+//let enemyObject = {speed:65,case1:false}
 let enemyCase1 = false
 let enemyCase2 = false
 let enemySpeed2 = 65 //enemy speed
@@ -237,49 +162,20 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function() {
 })
 
 //arrow shoot
-let projectile= sprites.createProjectileFromSprite(img`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-`, mySprite, 0, 0)
+let projectile= sprites.createProjectileFromSprite(assets.image`arrowNone`, mySprite, 0, 0)
 
 
 controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
     if (arrowVX== playerSpeed && arrow > 0) {
         music.zapped.play()
         arrow = arrow - 1 
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . .
-            . . f . . . . . . . . . . 1 . .
-            . . . f e e e e e e e e e 1 1 .
-            . . f . . . . . . . . . . 1 . .
-            . . . . . . . . . . . . . . . .
-        `, mySprite, playerSpeed, 0)
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowRight`, mySprite, playerSpeed, 0)
         projectile.setVelocity(110, 0)
     }
     if (arrowVX== -playerSpeed && arrow > 0) {
         music.zapped.play()
         arrow = arrow - 1 
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . .
-            . . 1 . . . . . . . . . . f . .
-            . 1 1 e e e e e e e e e f . . .
-            . . 1 . . . . . . . . . . f . .
-            . . . . . . . . . . . . . . . .
-        `, mySprite, -playerSpeed, 0)
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowLeft`, mySprite, -playerSpeed, 0)
         projectile.setVelocity(-110, 0)
     }
     if (arrowVY== 56.5625 && arrowVX == 56.5625 && arrow > 0) {
@@ -308,47 +204,13 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function() {
            if (arrowVY== playerSpeed && arrow > 0) {
                music.zapped.play()
                arrow = arrow - 1 
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . .
-            . . . . .
-            . f . f .
-            . . f . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . 1 1 1 .
-            . . 1 . .
-            . . . . .
-        `, mySprite, 0, playerSpeed)
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowDown`, mySprite, 0, playerSpeed)
         projectile.setVelocity(0, 110)
     }
     if (arrowVY== -playerSpeed && arrow > 0) {
         music.zapped.play()
         arrow = arrow - 1 
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . .
-            . . 1 . .
-            . 1 1 1 .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . e . .
-            . . f . .
-            . f . f .
-            . . . . .
-            . . . . .
-        `, mySprite, 0, -playerSpeed)
+        projectile =sprites.createProjectileFromSprite(assets.image`arrowUp`, mySprite, 0, -playerSpeed)
         projectile.setVelocity(0, -110)
     }
     if (arrowVY== 56.5625 && arrowVX == 56.5625 && arrow > 0) {
